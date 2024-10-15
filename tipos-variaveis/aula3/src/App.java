1ccb610617d5482211fe9511092381cb56c34bb5
Pablo Silva <pablosilvaaluno@outlook.com>
2024-10-15 11:12:56 -0300

    tipos e variaveis (aula3)

---
+++ tipos-variaveis/aula3/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        //VALORES DOS TIPOS EM JAVA
+        // byte	    8 bits	    -128 a 127
+        // short	16 bits	    -32.768 a 32.767
+        // int	    32 bits	    -2.147.483.648 a 2.147.483.647
+        // long	    64 bits	    -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+    }
+}
