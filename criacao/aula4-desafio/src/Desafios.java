dc5df660f9521f00463a43748d94e57e7ec4afbd
Pablo Silva <pablosilvaaluno@outlook.com>
2024-10-16 12:19:57 -0300

    Fazendo To Do

--- criacao/aula4-desafio/src/Desafios.java
+++ criacao/aula4-desafio/src/Desafios.java
@@ -1,9 +1,9 @@
 
 /* 
  * Criar uma aplicação que mostre resolva as seguintes situações:
- * Calcule as 4 operaçoes matemáticas: soma, subtração, divisão e multiplicação.
- * A partir da hora do dia, informe a mensagem adequada: "Bom dia" "Boa tarde" ou "Boa noite"
- * Calcule o valor final de um emprestimo, a partir do valor solicitado. Taxas e parcelas influenciam.
+ * [ X ] Calcule as 4 operaçoes matemáticas: soma, subtração, divisão e multiplicação.
+ * [  ] A partir da hora do dia, informe a mensagem adequada: "Bom dia" "Boa tarde" ou "Boa noite"
+ * [  ] Calcule o valor final de um emprestimo, a partir do valor solicitado. Taxas e parcelas influenciam.
  * Defina arbitramente as taxas que influenciam nos valores.
  */
 
