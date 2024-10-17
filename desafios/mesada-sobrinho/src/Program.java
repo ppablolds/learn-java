7fe9cd9f0847d1b5ff83cc8440dbf0645c687409
Pablo Silva <pablosilvaaluno@outlook.com>
2024-10-17 09:05:36 -0300

    desafio mesada

---
+++ desafios/mesada-sobrinho/src/Program.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+    
+public class Program {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int entrada = leitor.nextInt();
+        int mesada = 50;
+        
+        // TODO: Crie a condição necessária para que o sobrinho saiba quanto dinheiro irá juntar nos meses da entrada
+        
+        int mesadaTotal = entrada * mesada;
+        System.out.println(mesadaTotal);
+
+    }
+}
