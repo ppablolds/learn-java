eac710bd50e10fbf9a265de018ffb2fb1dbaf348
Pablo Silva <pablosilvaaluno@outlook.com>
2024-10-23 00:01:35 -0300

    Arrys com list

--- br/com/dio/collections/list/src/ExampleList.java
+++ br/com/dio/collections/list/src/ExampleList.java
@@ -1,31 +1,52 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExampleList {
+
     public static void main(String[] args) {
-        System.out.println("Crie uma lista e coloque as 7 notas: ");
-
-        List<Double> notas = new ArrayList<>();
-        notas.add(7.0);
-        notas.add(2.25);
-        notas.add(9.0);
-        notas.add(8.0);
-        notas.add(6.5);
-        notas.add(3.0);
-        notas.add(10.0);
-        System.out.println(notas.toString());
-
-        System.out.println("Me mostra a posição da nota 3: " + notas.indexOf(3.0));
-        System.out.println(notas);
-
-        System.out.println("Adicione na lista a nota 1.0 na posição 4: ");
-        notas.add(4, 1d);
-        System.out.println(notas);
-
-        System.out.println("Substitua o valor da nota 1.0 para 10.0: ");
-        notas.set(notas.indexOf(1.0), 10d);
-        System.out.println(notas);
-
-        System.out.println("Confira se a nota 5 está na lista: " + notas.contains(5d));
+        List<Gato> meusGatos = new ArrayList<>(){{
+            add(new Gato("Adele", 12, "Branca"));
+            add(new Gato("Casito", 6, "Preto"));
+            add(new Gato("Garfield", 10, "Laranja"));
+        }};
+        System.out.println(meusGatos);
     }
+}
+
+class Gato {
+
+    private String nome;
+    private int idade;
+    private String cor;
+
+    public Gato(String nome, int idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Gato { ");
+        sb.append("nome: ").append(nome);
+        sb.append(", idade: ").append(idade);
+        sb.append(", cor: ").append(cor);
+        sb.append(" }");
+        return sb.toString();
+    }
+
 }
